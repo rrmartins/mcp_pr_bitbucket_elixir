78d4b16d588f9a535463aba89263844f43295e7a
Rodrigo Martins <rrmartinsjg@gmail.com>
2025-08-13 16:40:10 +0100

    Add McpPrBitbucketElixir module with hello function and documentation

---
+++ lib/mcp_pr_bitbucket_elixir.ex
@@ -0,0 +1,18 @@
+defmodule McpPrBitbucketElixir do
+  @moduledoc """
+  Documentation for `McpPrBitbucketElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> McpPrBitbucketElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
