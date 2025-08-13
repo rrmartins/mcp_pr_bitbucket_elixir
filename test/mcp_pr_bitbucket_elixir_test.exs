ab1f7bcf1f9e99f2d09b6156de34f7c314cbe731
Rodrigo Martins <rrmartinsjg@gmail.com>
2025-08-13 16:40:41 +0100

    Add test for McpPrBitbucketElixir module to verify hello function

---
+++ test/mcp_pr_bitbucket_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule McpPrBitbucketElixirTest do
+  use ExUnit.Case
+  doctest McpPrBitbucketElixir
+
+  test "greets the world" do
+    assert McpPrBitbucketElixir.hello() == :world
+  end
+end
